aa791a6ea2ceb9812643b5e71ef8170c059e6d82
Alexph7 <ph757@Fabia>
2023-01-24 12:33:32 -0300

    Classe PrecificaçãoDAO
    Setar programação Na Caixa de Pesquisa e Exibir Resultados Na Tabela a Medida que usuario digitta os resultados vao aparecendo com a biblioteca rs2xml e evento keyRelease

--- src/main/java/DAO/PrecificacaoDAO.java
+++ src/main/java/DAO/PrecificacaoDAO.java
@@ -1,23 +1,31 @@
 package DAO;
 
 import DTO.PrecificacaoDTO;
+import VIEW.TelaPrecificacao;
+import java.awt.HeadlessException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import net.proteanit.sql.DbUtils;
 
 public class PrecificacaoDAO {
 
-    Connection conexao;
-    PreparedStatement pstm;
+    Connection conexao = null;
+    PreparedStatement pstm = null;
+    ResultSet rs = null;
 
     /**
      * Classe operação de CRUD "Cadastrar", pega informações do objeto
      * relacionado e cadastra no Banco de Dados.
-     * @objPrecificacaoDTO passa um objeto preenchido do formulario com valores
-     * passado pelo usuario
+     * @param objPrecificacaoDTO passa um objeto preenchido do formulario com
+     * valores passado pelo usuario de um objeto DTO
      */
     public void cadastrarProduto(PrecificacaoDTO objPrecificacaoDTO) {
+
         String sql = "insert into tbprodutos(nome,dimensoes,quantidade,preco,total,data_produto) values(?,?,?,?,?,?);";
+
         conexao = new ConexaoDAO().conexaoBD();
 
         try {
@@ -33,8 +41,25 @@ public class PrecificacaoDAO {
             pstm.execute();
             pstm.close();
             JOptionPane.showMessageDialog(null, "Produto Inserido Com Sucesso");
-        } catch (Exception e) {
+        } catch (HeadlessException | SQLException e) {
             JOptionPane.showMessageDialog(null, "Classe PrecificacaoDao, Metodo Cadastrar Produto" + e.getMessage());
+        }
+    }
+
+    public void pesquisarProduto() {
+
+        String sql = "select idproduto as Id, nome as Nome, dimensoes as Dimensoes, quantidade as Quantidade, preco as Preço, total as Total, data_produto as Data from tbprodutos where nome like ?";
+
+        conexao = new ConexaoDAO().conexaoBD();
+
+        try {
+            pstm = conexao.prepareStatement(sql);
+            pstm.setString(1, TelaPrecificacao.txtPesquisaProduto.getText() + "%");
+            rs = pstm.executeQuery();
+            TelaPrecificacao.tblProdutos.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Classe PrecificacaoDao, Metodo Pesquisar Produto" + e.getMessage());
+
         }
     }
 
